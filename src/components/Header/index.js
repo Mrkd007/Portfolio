7cfdaafa41b79f505c66f829d10e7caaf62a342a
unknown <itskd17@gmail.com>
2021-03-30 21:54:11 +0530

    Fix: broken header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,32 +11,30 @@ import Logo from '../../Assets/images/logo.png';
 
 const Header = (props) => {
   return(
-    <header>
-        <nav class="body_navbar">
-            <div class="body_navbar--container">
-                <div class="body_navbar--container-header">
-                    <img heoght="42" width="42" className="body_navbar--container-header-logo" src={Logo} alt="KD"/>
-                </div>
-                <ul class="body_navbar--container-list">
-                    <li class="body_navbar--container-list-item active">
-                        <span className="body_navbar--container-list-item-title">Home</span>
-                        <span className="body_navbar--container-list-item-cover"></span>
-                    </li>
-                    <li class="body_navbar--container-list-item active">
-                        <span className="body_navbar--container-list-item-title">About</span>
-                        <span className="body_navbar--container-list-item-cover"></span>
-                    </li>
-                    <li class="body_navbar--container-list-item active">
-                        <span className="body_navbar--container-list-item-title">Project</span>
-                        <span className="body_navbar--container-list-item-cover"></span>
-                    </li>
-                    <li class="body_navbar--container-list-item active">
-                        <span className="body_navbar--container-list-item-title">Contact</span>
-                        <span className="body_navbar--container-list-item-cover"></span>
-                    </li>
-                </ul>
+    <header className="body_header">
+        <div className="body_header--container">
+            <div className="body_header--container-header">
+                <img height="42" width="42" className="body_header--container-header-logo" src={Logo} alt="KD"/>
             </div>
-        </nav>
+            <ul className="body_header--container-list">
+                <li className="body_header--container-list-item active">
+                    <span className="body_header--container-list-item-title">Home</span>
+                    <span className="body_header--container-list-item-cover"></span>
+                </li>
+                <li className="body_header--container-list-item">
+                    <span className="body_header--container-list-item-title">About</span>
+                    <span className="body_header--container-list-item-cover"></span>
+                </li>
+                <li className="body_header--container-list-item">
+                    <span className="body_header--container-list-item-title">Project</span>
+                    <span className="body_header--container-list-item-cover"></span>
+                </li>
+                <li className="body_header--container-list-item">
+                    <span className="body_header--container-list-item-title">Contact</span>
+                    <span className="body_header--container-list-item-cover"></span>
+                </li>
+            </ul>
+        </div>
     </header>
    )
 
