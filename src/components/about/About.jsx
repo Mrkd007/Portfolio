c5c81e76cc517b5fed8afdbfb86f46c6f7e7baa4
Mrkd007 <itskd17@gmail.com>
2022-02-22 09:16:10 +0530

    Update added all componetes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import './about.css'
+import React from 'react';
+import './about.css';
+import ME from '../../assets/kd.png';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="conatiner about__container">
+        <div className="about__me">
+          <div className="about__me-img">
+            <img src={ME} alt="Profile" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>100+ clients</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon' />
+              <h5>Project Completed</h5>
+              <small>100+ completed projects</small>
+            </article>
+          </div>
+
+          <p>
+            Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except eject will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
